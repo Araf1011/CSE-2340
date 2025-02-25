b32afd4de27f95005d2d6935a66a613e8fcc11fc
Mohammad Araf <159543785+Araf1011@users.noreply.github.com>
2025-02-25 11:52:38 +0600

    Create test1.java

---
+++ test1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] marks = new int[]{85, 65, 84, 75, 68};
+
+        String [] name = { "Karim", "Rahim", "Sakib", "Mamun", "Nafis"};
+
+        for(int i=0;i<5;i++) {
+            System.out.println("Enter marks for " +name[i] + " : " + marks[i]);
+        }
+
+        System.out.println("Enter a student name to check the average :");
+        String studentName = sc.nextLine();
+
+        int total = 0;
+        for(int i=0;i<5;i++) {
+            total += marks[i];
+        }
+        double average = total / 5;
+        System.out.println("Class Average marks : " + average);
+    }
+}
