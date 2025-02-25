c0660779c3cde97adb581845045ec38ad59f35d1
Mohammad Araf <159543785+Araf1011@users.noreply.github.com>
2025-02-25 12:14:33 +0600

    Update test1.java

--- test1.java
+++ test1.java
@@ -4,20 +4,29 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[] marks = new int[]{85, 65, 84, 75, 68};
 
-        String [] name = { "Karim", "Rahim", "Sakib", "Mamun", "Nafis"};
+        String[] name = {"Karim", "Rahim", "Sakib", "Mamun", "Nafis" };
 
-        for(int i=0;i<5;i++) {
-            System.out.println("Enter marks for " +name[i] + " : " + marks[i]);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter marks for " + name[i] + " : " + marks[i]);
         }
 
-        System.out.println("Enter a student name to check the average :");
-        String studentName = sc.nextLine();
-
         int total = 0;
-        for(int i=0;i<5;i++) {
+        for (int i = 0; i < 5; i++) {
             total += marks[i];
         }
         double average = total / 5;
         System.out.println("Class Average marks : " + average);
+
+        System.out.println("Enter a student name to check the average : ");
+        String studentName = sc.nextLine();
+
+        for (int i = 0; i < 5; i++) {
+            if (studentName.equals(name[i])) {
+                System.out.println(name[i] + " 's mark is "  + marks[i]);
+            }
+        }
+
+
+
     }
 }
